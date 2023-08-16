b25b91a9a669ca2940dc6ea3e03cc74a857e236a
Umesh <umeshnagar9152@gmail.com>
2023-08-16 22:16:44 +0530

    React-Cryto-App

---
+++ src/components/Footer.jsx
@@ -0,0 +1,38 @@
+import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+
+const avatarSrc = "https://i.pinimg.com/564x/25/01/cd/2501cd527aba3b6366354c674103f6be.jpg";
+
+const Footer = () => {
+  return (
+    <Box
+      bgColor={"blackAlpha.900"}
+      color={"whiteAlpha.700"}
+      minH={"48"}
+      px={"16"}
+      py={["16", "8"]}
+    >
+      <Stack direction={["column", "row"]} h={"full"} alignItems={"center"}>
+        <VStack w={"full"} alignItems={["center", "flex-start"]}>
+          <Text fontWeight={"bold"}>About Us</Text>
+          <Text
+            fontSize={"sm"}
+            letterSpacing={"widest"}
+            textAlign={["center", "left"]}
+          >
+            We are the best crypto trading app in India, we provide our guidance
+            at a very cheap price.
+          </Text>
+        </VStack>
+
+        <VStack>
+        <Text>Site Developed By</Text>
+          <Avatar boxSize={"28"} mt={["4", "0"]} src={avatarSrc} />
+         
+        </VStack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
